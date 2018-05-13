2f114ece262df16407f96bbf222cd33270413645
doovemax <445001843@qq.com>
2018-05-13 22:53:26 +0800

    no message

--- core/args.go
+++ core/args.go
@@ -15,6 +15,6 @@ func init() {
 	flag.StringVar(&f, "f", "", "specify config file")
 	flag.StringVar(&host, "host", "", "specity remote host")
 	flag.IntVar(&port, "port", 22, "specity remote port")
-	flag.BoolVar(&list, "list", flase, "list remote hosts")
+	flag.BoolVar(&list, "list", false, "list remote hosts")
 
 }
